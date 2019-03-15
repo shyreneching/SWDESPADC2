7fdcbf991357a682a7e4467ce9de6af73e1de9bf
Erika <erika_y_chan@dlsu.edu.ph>
2019-03-15 14:24:05 +0800

    Added more stuff

--- src/View/Main.java
+++ src/View/Main.java
@@ -2,6 +2,7 @@ package View;
 
 import javafx.stage.*;
 
+
 import java.io.IOException;
 
 import javafx.application.*;
@@ -13,7 +14,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-    	LoginView view = new LoginView(primaryStage);
+    	//LoginView view = new LoginView(primaryStage);
+    	try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Login.fxml"));
+            loader.setController(this);
+            Parent root = (Parent) loader.load();
+            System.out.println("test");
+            Scene scene = new Scene(root);
+            
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+        } catch (IOException ie) {
+        }
     }
 
     public static void main(String[] args) {
